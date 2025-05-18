8987ec76165f3569ec7e8fcb2f143b566b193214
nkaewam <tontan2545@gmail.com>
2025-05-18 15:41:00 +0700

    feat: add allow origin prod frontend

--- scdl-backend/main.py
+++ scdl-backend/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 # Add CORS middleware to allow requests from http://localhost:3000
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://soundcloudy.nkaeawm.dev"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
