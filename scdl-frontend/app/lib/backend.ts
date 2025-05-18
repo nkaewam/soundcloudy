ee3b940a84e54293ae5c951cb5f27f96e86fe382
nkaewam <tontan2545@gmail.com>
2025-05-18 15:15:50 +0700

    feat: finalized

--- scdl-frontend/app/lib/backend.ts
+++ scdl-frontend/app/lib/backend.ts
@@ -1,4 +1,4 @@
-const BACKEND_URL = "http://localhost:8000";
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 export const downloadSoundCloudTrack = async (url: string) => {
   return fetch(`${BACKEND_URL}/download/${url}`);
